6a83a9b014f3d70c41eb342103610748432dbfef
Ray <raysidarta@gmail.com>
2021-01-26 15:31:25 -0500

    Work on 03a_nobel_bonus

--- skeleton/lib/03a_nobel_bonus.rb
+++ skeleton/lib/03a_nobel_bonus.rb
@@ -12,24 +12,12 @@ def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
   SELECT
-    nb_physics.yr
-  FROM (
-    SELECT
-      *
-    FROM
-      nobels
-    WHERE
-      subject = 'Physics'
-  ) AS nb_physics
-  LEFT OUTER JOIN (
-    SELECT
-      *
-    FROM
-      nobels
-    WHERE
-      subject = 'Chemistry'
-  ) AS nb_Chemistry ON nb_physics = nb_Chemistry
-  WHERE
-    nb_Chemistry.yr ISNULL
+    yr
+  FROM
+    nobels 
+  WHERE 
+    subject != 'Chemistry' AND subject = 'Physics'
+
+    
   SQL
 end
